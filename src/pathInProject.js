23713dafbe2dfa5ed898bedb6b208c6c7e934626
Andy Edwards <jedwards@fastmail.com>
2018-07-29 13:45:22 -0500

    fix(pathInProject): fix same directory edge case

--- src/pathInProject.js
+++ src/pathInProject.js
@@ -3,5 +3,6 @@ const findRoot = require('find-root')
 
 module.exports = function pathInProject(file, ...paths) {
   const target = path.resolve(findRoot(file), ...paths)
+  if (path.dirname(file) === path.dirname(target)) return `./${path.basename(target)}`
   return path.relative(path.dirname(file), target)
 }
