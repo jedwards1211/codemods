e0e616fbe65c04c3763485b3807c9523f5fd0f99
Andy Edwards <jedwards@fastmail.com>
2018-07-29 02:26:29 -0500

    fix(unwrapJSXElement): replace with React.Fragment if impossible to remove altogether

--- src/unwrapJSXElement.js
+++ src/unwrapJSXElement.js
@@ -1,9 +1,29 @@
 const j = require('jscodeshift').withParser('babylon')
+const isChildJSXElement = require('./isChildJSXElement')
 
 module.exports = function wrapWithJSXElement({
   root,
   filter = () => true,
 }) {
   const element = root.find(j.JSXElement).filter(filter).at(0)
-  element.replaceWith(path => path.node.children)
+  const path = element.paths()[0]
+  const {node} = path
+  if (!isChildJSXElement(path) && node.children.length > 1) {
+    node.openingElement = j.jsxOpeningElement(
+      j.jsxMemberExpression(
+        j.jsxIdentifier('React'),
+        j.jsxIdentifier('Fragment')
+      ),
+      [],
+      false
+    )
+    node.closingElement = j.jsxClosingElement(
+      j.jsxMemberExpression(
+        j.jsxIdentifier('React'),
+        j.jsxIdentifier('Fragment')
+      )
+    )
+  } else {
+    element.replaceWith(path => path.node.children)
+  }
 }
